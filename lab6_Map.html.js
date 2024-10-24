da9f0688379aac222fb218796a6d26250eef08a2
DavidLin29 <dlin12@terpmail.umd.edu>
2024-10-23 20:48:16 -0400

    Update: I added the map of the US YAY

---
+++ lab6_Map.html.js
@@ -0,0 +1,12 @@
+function createMap() {
+    var map = L.map('map').setView([37.09024, -95.712891], 5);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
+
+}
+
+window.onload=createMap;
