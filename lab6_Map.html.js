cfbadf984d67e1f321e829d7ac98194c761140a5
DavidLin29 <dlin12@terpmail.umd.edu>
2024-10-23 21:46:43 -0400

    Update: Added locality Function, still some bugs to fix though

--- lab6_Map.html.js
+++ lab6_Map.html.js
@@ -5,8 +5,39 @@ function createMap() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    
+
+    return map;
+
 
 }
 
-window.onload=createMap;
+
+function getRandomInRange(from, to, fixed) {
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+    // .toFixed() returns string, so ' * 1' is a trick to convert to number
+}
+
+function mapWithCordinates(){
+    const map = createMap();
+    const coordinates = [
+        {lat: getRandomInRange(30, 35, 3), long: getRandomInRange(-90, -100, 3)},
+        {lat: getRandomInRange(30, 35, 3), long: getRandomInRange(-90, -100, 3)},
+        {lat: getRandomInRange(30, 35, 3), long: getRandomInRange(-90, -100, 3)}
+    ];
+
+    coordinates.forEach((coord, index) => {
+        const marker = L.marker([coord.lat, coord.long]).addTo(map);
+    document.getElementById(`latAndLong${index + 1}`).textContent = `Marker 1: Latitude: ${coord.lat}, Longitude: ${coord.long}`;
+
+    fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en')
+        .then(res => res.json())
+        .then(data => {
+            const locality = data.locality;
+            document.getElementById(`locality${index + 1}`).textContent = `Locality: ${locality}`;
+        })
+
+    })
+
+}
+
+window.onload = mapWithCordinates;
